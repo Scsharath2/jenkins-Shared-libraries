5e034527f3447a946c79c537a626ff6cc5f446ff
Sharath chandra Gajjela <SGAJJELA@amdocs.com>
2022-05-31 13:59:29 +0530

    Practice

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
-#https://opensource.triology.de/jenkins/pipeline-syntax/globals
+# https://opensource.triology.de/jenkins/pipeline-syntax/globals
+# Following code will check the warnings and based on occurnace it will set the status of the jfPipelines
 package org.apache.commons.lang
 public class StringUtils
 
-def call(String filter_string, int count){
+def call(String filter_string, int occurrence){
     def buildLog  = currentBuild. rawBuild.log(1000).join(\n)
     def count = StringUtils.CountMatches(buildlog,filter_string)
     if (count > occurrence-1){
